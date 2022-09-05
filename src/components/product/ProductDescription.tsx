4adb4c6f08c7184a4e7c943a4d1238b61c352db6
Max Edwards <max@solotodo.com>
2022-09-05 17:20:54 -0400

    use detail template in category

--- src/components/product/ProductDescription.tsx
+++ src/components/product/ProductDescription.tsx
@@ -1,31 +1,21 @@
 import { Product } from "src/frontend-utils/types/product";
 import { useApiResourceObjects } from "src/frontend-utils/redux/api_resources/apiResources";
 import { useAppSelector } from "src/store/hooks";
-import { constants } from "src/config";
 import { Typography } from "@mui/material";
 import styles from "../../styles/ProductPage.module.css";
 import Handlebars from "handlebars";
-import { useEffect, useState } from "react";
-import { fetchJson } from "src/frontend-utils/network/utils";
+import { Category } from "src/frontend-utils/types/store";
 
 export default function ProductDescription({ product }: { product: Product }) {
   const apiResourceObjects = useAppSelector(useApiResourceObjects);
-  const [template, setTemplate] = useState<{ body: string } | null>(null);
+  const category = apiResourceObjects[product.category] as Category;
 
-  useEffect(() => {
-    fetchJson(
-      `${
-        constants.apiResourceEndpoints.category_templates
-      }?website=2&purpose=1&category=${apiResourceObjects[product.category].id}`
-    ).then((category_template) => {
-      category_template.length !== 0 && setTemplate(category_template[0]);
-    });
-  }, [apiResourceObjects, product.category, product.id]);
+  const template = category.detail_template;
 
   const formatSpecs = () => {
     let html = "";
     if (template) {
-      const templateHandler = Handlebars.compile(template.body);
+      const templateHandler = Handlebars.compile(template);
       html = templateHandler(product.specs);
     }
 
