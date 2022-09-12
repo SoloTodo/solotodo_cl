140cd259611a69d41c84a1b80c1d646c8e8dfc16
Max Edwards <max@solotodo.com>
2022-09-12 16:20:09 -0300

    add subtitles

--- src/categorySlides.ts
+++ src/categorySlides.ts
@@ -5,19 +5,19 @@ export const categorySlides: NavigationItemProps[] = [
     name: "PS5",
     path: "https://www.solotodo.cl/consoles?families=1182932",
     picture: "https://media.solotodo.com/media/nav_items/ps5.png",
-    subtitle: "El futuro es hoy",
+    subtitle: "Lo más reciente de Sony",
   },
   {
     name: "iPhone",
     path: "https://www.solotodo.cl/cells?brands=149039",
     picture: "https://media.solotodo.com/media/nav_items/iphone.png",
-    subtitle: "El futuro es hoy",
+    subtitle: "El equipo estrella de Apple",
   },
   {
     name: "Smartphones con 5G",
     path: "https://www.solotodo.cl/cells?min_network_generation_start=1123014",
     picture: "https://media.solotodo.com/media/nav_items/5g_W16hqGL.png",
-    subtitle: "",
+    subtitle: "Conéctate a la red más rápida",
   },
   {
     name: "Smart TV",
