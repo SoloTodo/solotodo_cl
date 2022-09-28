96af6e983e6b190a1e80691105d7bdc5e9eb0680
Max Edwards <max@solotodo.com>
2022-09-28 17:21:51 -0300

    add redirects

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["media.solotodo.com"],
-  }
+  },
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/tos',
+        destination: '/legal_information?tab=0',
+        permanent: true,
+      },
+      {
+        source: '/privacy_policy',
+        destination: '/legal_information?tab=2',
+        permanent: true,
+      }
+    ]
+  },
+}
