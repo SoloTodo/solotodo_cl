2013671a72803470b87e2847b7532caa8366a03a
Max Edwards <max@solotodo.com>
2022-10-03 14:43:57 -0300

    add verify email

---
+++ src/pages/account/verify-email.tsx
@@ -0,0 +1,62 @@
+import { Container } from "@mui/material";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
+import Page from "src/components/Page";
+import { fetchJson } from "src/frontend-utils/network/utils";
+import { wrapper } from "src/store/store";
+
+export default function VerifyEmail() {
+  const { enqueueSnackbar } = useSnackbar();
+  const router = useRouter();
+
+  useEffect(() => {
+    enqueueSnackbar(
+      "Ocurrió un error al verificar tu correo, por favor verifica el link que seguiste. Si el problema persiste contáctanos!",
+      { persist: true, variant: "error" }
+    );
+    router.push("/");
+  }, [enqueueSnackbar, router]);
+
+  return (
+    <Page title="Verificando cuenta">
+      <Container maxWidth={false}></Container>
+    </Page>
+  );
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (st) => async (context) => {
+    const user = st.getState().user;
+    if (user) {
+      context.res.writeHead(302, {
+        Location: "/",
+      });
+      context.res.end();
+    }
+
+    let response = null;
+    try {
+      response = await fetchJson("rest-auth/registration/verify-email/", {
+        method: "post",
+        body: JSON.stringify({ key: context.query["key"] }),
+      });
+    } catch {
+      context.res.writeHead(302, {
+        Location: "/",
+      });
+      context.res.end();
+    }
+
+    if (response["detail"] === "Ok") {
+      context.res.writeHead(302, {
+        Location: "/login?post_verify=1",
+      });
+      context.res.end();
+    }
+
+    return {
+      props: {},
+    };
+  }
+);
