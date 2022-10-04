1fa8ad3f16054d278fe5a80e9133574698daa177
Max Edwards <max@solotodo.com>
2022-10-04 17:00:11 -0300

    add carousel to más recientes

--- src/components/website-slides/RecentSlidesRow.tsx
+++ src/components/website-slides/RecentSlidesRow.tsx
@@ -1,26 +1,56 @@
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
+import Slider from "react-slick";
 import RecentCard from "./RecentCard";
 import { Slide } from "./types";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function RecentSlidesRow({
   recentSlides,
 }: {
   recentSlides: Slide[];
 }) {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <Grid
-      container
-      spacing={3}
-      wrap="nowrap"
-      overflow="auto"
+    <Box
+      sx={{
+        maxWidth: 1270,
+        marginBottom: 1,
+        marginX: "auto"
+      }}
     >
-      {recentSlides.map((d, index) => {
-        return (
-          <Grid item key={index}>
-            <RecentCard recentData={d} />
-          </Grid>
-        );
-      })}
-    </Grid>
+      <Slider {...settings}>
+        {recentSlides.map((d, index) => (
+          <RecentCard key={index} recentData={d} />
+        ))}
+      </Slider>
+    </Box>
   );
 }
