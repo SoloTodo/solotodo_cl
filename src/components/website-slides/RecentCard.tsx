1950b5dd01f3975c369490f45a12bdeabdc1586e
Max Edwards <max@solotodo.com>
2022-10-06 15:31:54 -0300

    refactor card max width

--- src/components/website-slides/RecentCard.tsx
+++ src/components/website-slides/RecentCard.tsx
@@ -18,7 +18,7 @@ export default function RecentCard({ recentData }: { recentData: Slide }) {
   return (
     <Box
       sx={{
-        maxWidth: { xs: "90%", sm: 395 },
+        maxWidth: { xs: "90%", sm: "80%", lg: 395 },
         height: { xs: 150, sm: 170 },
         paddingX: 2,
         marginY: 1,
