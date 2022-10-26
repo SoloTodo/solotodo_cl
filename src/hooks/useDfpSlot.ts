21bcecbae2ac7eb10ef044949eb646bfd8ed3713
Max Edwards <max@solotodo.com>
2022-10-26 17:15:06 -0300

    resize add for mobile

--- src/hooks/useDfpSlot.ts
+++ src/hooks/useDfpSlot.ts
@@ -1,7 +1,10 @@
+import { useMediaQuery, useTheme } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export const useDfpSlot = (category: string, divId: string) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const router = useRouter();
   const [transition, setTransition] = useState(false);
 
@@ -20,15 +23,13 @@ export const useDfpSlot = (category: string, divId: string) => {
 
     if (!transition) {
       const googletag = (window as any).googletag || {};
+      const sizes = isMobile ? [[320, 50], [300, 50]] : [[728,90], [970, 90]];
+
       googletag.cmd.push(function () {
         googletag
           .defineSlot(
             "/21667261583/top_banner",
-            [
-              [970, 90],
-              [728, 90],
-              [320, 50],
-            ],
+            sizes,
             divId
           )
           .setTargeting("category", [category])
@@ -45,5 +46,6 @@ export const useDfpSlot = (category: string, divId: string) => {
       router.events.off("routeChangeStart", setTransitionStarted);
       router.events.off("routeChangeComplete", setTransitionComplete);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [category, divId, router.events, transition]);
 };
