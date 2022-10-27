13d693b734d9519787804e704e42fdce8c7ff784
Max Edwards <max@solotodo.com>
2022-10-27 15:45:47 -0300

    use Roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -52,7 +52,7 @@ export default class MyDocument extends Document {
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
 
