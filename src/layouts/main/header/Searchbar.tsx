1eb1fde2dde683e91fbe68145d0ec191a91e0628
Max Edwards <max@solotodo.com>
2022-11-02 17:18:56 -0300

    searchbar hide in mobile size

--- src/layouts/main/header/Searchbar.tsx
+++ src/layouts/main/header/Searchbar.tsx
@@ -1,16 +1,26 @@
 import * as Yup from "yup";
 import { useState } from "react";
 // @mui
-import { Input, InputAdornment } from "@mui/material";
+import {
+  ClickAwayListener,
+  Input,
+  InputAdornment,
+  Slide,
+  styled,
+} from "@mui/material";
 // components
 import Iconify from "../../../components/Iconify";
 // hooks
 import { useRouter } from "next/router";
 import { FormProvider } from "src/components/hook-form";
 import useSettings from "src/hooks/useSettings";
+import useResponsive from "src/hooks/useResponsive";
 // form
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import cssStyles from "src/utils/cssStyles";
+import { IconButtonAnimate } from "src/components/animate";
+import { HEADER } from "src/config";
 
 // ----------------------------------------------------------------------
 
@@ -20,10 +30,38 @@ type FormValuesProps = {
 
 // ----------------------------------------------------------------------
 
+const SearchbarStyle = styled("div")(({ theme }) => ({
+  ...cssStyles(theme).bgBlur(),
+  top: 0,
+  left: 0,
+  zIndex: 99,
+  width: "100%",
+  display: "flex",
+  position: "absolute",
+  alignItems: "center",
+  height: HEADER.MOBILE_HEIGHT,
+  padding: theme.spacing(0, 3),
+  boxShadow: theme.customShadows.z8,
+  [theme.breakpoints.up("md")]: {
+    height: HEADER.DASHBOARD_DESKTOP_HEIGHT,
+    padding: theme.spacing(0, 5),
+  },
+}));
+
 export default function Searchbar() {
+  const [isOpen, setOpen] = useState(false);
   const [keywords, setKeywords] = useState("");
   const router = useRouter();
   const settings = useSettings();
+  const isDesktop = useResponsive("up", "md");
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const SearchSchema = Yup.object().shape({
     search: Yup.string(),
@@ -48,35 +86,54 @@ export default function Searchbar() {
     );
   };
 
+  const SearchInput = (
+    <Input
+      name="search"
+      disableUnderline
+      placeholder="Busca un producto"
+      endAdornment={
+        <InputAdornment position="end">
+          <Iconify
+            icon={"eva:search-fill"}
+            sx={{ color: "text.disabled", width: 20, height: 20 }}
+          />
+        </InputAdornment>
+      }
+      sx={{
+        fontWeight: "fontWeightBold",
+        paddingY: 0.5,
+        paddingX: 1,
+        display: "flex",
+        width: isDesktop ? 300 : "100%",
+        borderRadius: "4px",
+        border:
+          settings.themeMode === "dark"
+            ? "1px solid rgba(255, 255, 255, 0.23)"
+            : "1px solid rgba(0, 0, 0, 0.23)",
+        backgroundColor: "background.default",
+      }}
+      onChange={(evt) => setKeywords(evt.target.value)}
+    />
+  );
+
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        name="search"
-        disableUnderline
-        placeholder="Busca un producto"
-        endAdornment={
-          <InputAdornment position="end">
-            <Iconify
-              icon={"eva:search-fill"}
-              sx={{ color: "text.disabled", width: 20, height: 20 }}
-            />
-          </InputAdornment>
-        }
-        sx={{
-          fontWeight: "fontWeightBold",
-          paddingY: 0.5,
-          paddingX: 1,
-          display: "flex",
-          width: 300,
-          borderRadius: "4px",
-          border:
-            settings.themeMode === "dark"
-              ? "1px solid rgba(255, 255, 255, 0.23)"
-              : "1px solid rgba(0, 0, 0, 0.23)",
-          backgroundColor: "background.default"
-        }}
-        onChange={(evt) => setKeywords(evt.target.value)}
-      />
-    </FormProvider>
+    <ClickAwayListener onClickAway={handleClose}>
+      <div>
+        {!isDesktop && !isOpen && (
+          <IconButtonAnimate onClick={handleOpen}>
+            <Iconify icon={"eva:search-fill"} width={20} height={20} />
+          </IconButtonAnimate>
+        )}
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+          {isDesktop ? (
+            SearchInput
+          ) : (
+            <Slide direction="down" in={isOpen} mountOnEnter unmountOnExit>
+              <SearchbarStyle>{SearchInput}</SearchbarStyle>
+            </Slide>
+          )}
+        </FormProvider>
+      </div>
+    </ClickAwayListener>
   );
 }
