884e798f150f2f183afb6a9c535ee4f4a436dec4
Max Edwards <max@solotodo.com>
2022-12-13 17:12:37 -0300

    similar to 404

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,50 +1,86 @@
-import { m } from 'framer-motion';
+import { m } from "framer-motion";
 // next
-import NextLink from 'next/link';
+import NextLink from "next/link";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Box, Button, Typography, Container } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Button, Typography, Container, Grid } from "@mui/material";
 // layouts
-import Layout from '../layouts';
+import Layout from "../layouts";
 // components
-import Page from '../components/Page';
-import { MotionContainer, varBounce } from '../components/animate';
+import Page from "../components/Page";
+import { MotionContainer, varBounce } from "../components/animate";
 // assets
-import { SeverErrorIllustration } from '../assets';
+import Image from "next/image";
+import { SeverErrorIllustration } from "../assets";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  display: 'flex',
-  height: '100%',
-  alignItems: 'center',
+const RootStyle = styled("div")(({ theme }) => ({
+  display: "flex",
+  height: "100%",
+  alignItems: "center",
   paddingTop: theme.spacing(15),
   paddingBottom: theme.spacing(10),
 }));
 
 export default function Page500() {
   return (
-    <Page title="500 Internal Server Error" sx={{ height: 1 }}>
+    <Page title="404 Page Not Found" sx={{ height: 1 }}>
       <RootStyle>
         <Container component={MotionContainer}>
-          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
-            <m.div variants={varBounce().in}>
-              <Typography variant="h3" paragraph>
-                500 Internal Server Error
-              </Typography>
-            </m.div>
-            <Typography sx={{ color: 'text.secondary' }}>
-              There was an error, please try again later.
-            </Typography>
-            <m.div variants={varBounce().in}>
-              <SeverErrorIllustration sx={{ height: 260, my: { xs: 5, sm: 10 } }} />
-            </m.div>
-            <NextLink href="/">
-              <Button size="large" variant="contained">
-                Go to Home
-              </Button>
-            </NextLink>
-          </Box>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Box sx={{ maxWidth: 480, margin: "auto" }}>
+                <Typography variant="h4" sx={{ color: "text.secondary" }}>
+                  Error 500
+                </Typography>
+                <m.div variants={varBounce().in}>
+                  <Typography variant="h1">
+                    Error interno del servidor
+                  </Typography>
+                </m.div>
+                <Typography sx={{ color: "text.secondary" }}>
+                  Ocurrió un error, por favor intenta denuevo más tarde
+                </Typography>
+                <br />
+                <NextLink href="/">
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="secondary"
+                    sx={{ borderRadius: 3 }}
+                  >
+                    VOLVER AL HOME
+                  </Button>
+                </NextLink>
+              </Box>
+            </Grid>
+            <Grid item>
+              <m.div variants={varBounce().in}>
+                <Box
+                  borderRadius={15}
+                  marginTop={5}
+                  sx={{
+                    position: "absolute",
+                    width: "240px",
+                    height: "240px",
+                    background: "rgba(205, 97, 49, 0.2)",
+                  }}
+                />
+                <Image
+                  src={"/BasicMoves2.png"}
+                  alt={"Página no encontrada"}
+                  width={"257px"}
+                  height={"395px"}
+                />
+              </m.div>
+            </Grid>
+          </Grid>
         </Container>
       </RootStyle>
     </Page>
